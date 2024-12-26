ccc018f2ebe3e839c7dbc7693c1db147acdfc369
ro-vezur <authenttesting@gmail.com>
2024-12-26 14:20:39 +0200

    feat: result resource

---
+++ app/src/main/java/com/example/jetmusic/Resources/ResultResource.kt
@@ -0,0 +1,7 @@
+package com.example.jetmusic.Resources
+
+sealed class ResultResource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : ResultResource<T>(data)
+    class Error<T>(message: String, data: T? = null) : ResultResource<T>(data, message)
+    class Loading<T>(data: T? = null) : ResultResource<T>(data)
+}
