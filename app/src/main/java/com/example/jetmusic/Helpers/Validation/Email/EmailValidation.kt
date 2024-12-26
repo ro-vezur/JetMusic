7a37fab8cb8048282dea1ad7808fedf849e79f69
ro-vezur <authenttesting@gmail.com>
2024-12-26 14:08:15 +0200

    feat: email validation

--- app/src/main/java/com/example/jetmusic/Helpers/Validation/Email/EmailValidation.kt
+++ app/src/main/java/com/example/jetmusic/Helpers/Validation/Email/EmailValidation.kt
@@ -3,12 +3,13 @@ package com.example.jetmusic.Helpers.Validation.Email
 import android.util.Patterns
 import com.example.jetmusic.Helpers.Validation.Result.ValidationResults
 
-class Email {
+class EmailValidation {
     companion object {
-        fun validate(email: String): ValidationResults {
+        fun validate(email: String,additionalValidators: Boolean = false): ValidationResults {
             return when {
                 email.isEmpty() -> ValidationResults.ERROR
                 !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> ValidationResults.ERROR
+                additionalValidators -> ValidationResults.ERROR
                 else -> ValidationResults.CORRECT
             }
         }
