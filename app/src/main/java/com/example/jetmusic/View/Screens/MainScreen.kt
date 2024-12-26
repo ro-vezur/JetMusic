74e9df20190cac2debb7b8c13a923816ada69186
ro-vezur <authenttesting@gmail.com>
2024-12-26 14:11:02 +0200

    feat: main screen

--- app/src/main/java/com/example/jetmusic/View/Screens/MainScreen.kt
+++ app/src/main/java/com/example/jetmusic/View/Screens/MainScreen.kt
@@ -10,9 +10,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.jetmusic.View.Components.Buttons.TextButton
+import com.example.jetmusic.View.Screens.StartScreen.HomeScreen.HomeScreen
 import com.example.jetmusic.View.Screens.StartScreen.startScreensGraph
 import com.example.jetmusic.View.ScreensRoutes
+import com.example.jetmusic.ViewModels.StartScreenViewModels.UserViewModel
 
 @Composable
 fun MainScreen() {
@@ -21,6 +27,11 @@ fun MainScreen() {
     val navController = rememberNavController()
 
     var showBottomBar by remember { mutableStateOf(false) }
+
+    val userViewModel: UserViewModel = hiltViewModel()
+    val firebaseUser by userViewModel.firebaseUser.collectAsStateWithLifecycle()
+
+    val initialRoute = if(firebaseUser != null) ScreensRoutes.HomeRoute else ScreensRoutes.StartScreens
 
     Scaffold(
         containerColor = colors.background,
@@ -36,12 +47,30 @@ fun MainScreen() {
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = ScreensRoutes.StartScreens
+            startDestination = initialRoute,
         ) {
             startScreensGraph(
                 navController = navController,
-                showBottomBar = { showBottomBar = it}
+                showBottomBar = { showBottomBar = it},
+                setUser = { newUser ->
+                    userViewModel.setUser(newUser)
+                }
             )
+
+            composable<ScreensRoutes.HomeRoute> {
+                showBottomBar = true
+
+                TextButton(
+                    text = "Log Out",
+                    onClick = {
+                        userViewModel.logOut()
+                    }
+                )
+
+                HomeScreen(
+
+                )
+            }
         }
     }
 }
