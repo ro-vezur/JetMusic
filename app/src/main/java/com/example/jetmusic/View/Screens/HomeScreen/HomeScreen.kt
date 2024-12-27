c0405e42814c839374a12a5c7f9b38a1c59bbe56
ro-vezur <authenttesting@gmail.com>
2024-12-27 23:59:52 +0200

    feat: home screen

---
+++ app/src/main/java/com/example/jetmusic/View/Screens/HomeScreen/HomeScreen.kt
@@ -0,0 +1,164 @@
+package com.example.jetmusic.View.Screens.HomeScreen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
+import com.example.jetmusic.DTOs.UserDTOs.User
+import com.example.jetmusic.Resources.ResultResource
+import com.example.jetmusic.View.Components.Cards.MusicCards.MusicCard
+import com.example.jetmusic.View.Components.TabsRow.CustomScrollableTabRow
+import com.example.jetmusic.View.Screens.HomeScreen.TabsCategories.TabsHomeCategories
+import com.example.jetmusic.ViewModels.MainScreensViewModels.HomeViewModel
+import com.example.jetmusic.ui.theme.darkGrey
+import com.example.jetmusic.ui.theme.typography
+import ir.kaaveh.sdpcompose.sdp
+
+@Composable
+fun HomeScreen(
+    navController: NavController,
+    user: User,
+    homeViewModel: HomeViewModel = hiltViewModel(),
+) {
+
+    val pagerState = rememberPagerState { TabsHomeCategories.entries.size }
+
+    val musicOfWeek by homeViewModel.musicOfWeek.collectAsStateWithLifecycle()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorScheme.background),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(top = 20.sdp)
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(start = 12.sdp),
+                verticalArrangement = Arrangement.spacedBy(5.sdp)
+            ) {
+                Text(
+                    text = "Hi ${user.fullName},",
+                    style = typography().titleSmall,
+                    fontWeight = FontWeight.W400,
+                    color = Color.LightGray.copy(0.9f)
+                )
+
+                Text(
+                    text = "Good evening",
+                    style = typography().titleMedium
+                )
+            }
+        }
+
+        CustomScrollableTabRow(
+            modifier = Modifier
+                .padding(top = 20.sdp, start = 6.sdp, end = 6.sdp)
+                .fillMaxWidth(),
+            pagerState = pagerState,
+            items = TabsHomeCategories.entries.map { it.title },
+            textColor = Color.Gray,
+            containerColor = darkGrey,
+            onClick = { index ->
+                homeViewModel.setMusicOfWeekLoading()
+                homeViewModel.getMusicOfWeek(
+                    tags = TabsHomeCategories.entries[index].genreId.toString(),
+                    offset = 0
+                )
+            }
+        )
+
+        HorizontalPager(
+            userScrollEnabled = false,
+            state = pagerState,
+            modifier = Modifier
+                .padding(top = 18.sdp)
+                .fillMaxWidth(0.9f)
+                .fillMaxHeight(0.78f)
+                .shadow(8.sdp, shape = RoundedCornerShape(14.sdp))
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(Color.DarkGray, colorScheme.background)
+                    )
+                )
+        ) {
+
+                when (musicOfWeek) {
+                    is ResultResource.Loading -> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ){
+                            CircularProgressIndicator()
+                        }
+                    }
+
+                    is ResultResource.Success -> {
+                        musicOfWeek.data?.let { musicResponse ->
+                            LazyColumn(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.spacedBy(10.sdp)
+                            ) {
+                                item {  }
+
+                                items(musicResponse.results) { music ->
+                                    MusicCard(
+                                        modifier = Modifier
+                                            .padding(start = 10.sdp)
+                                            .height(46.sdp)
+                                            .fillMaxWidth(),
+                                        musicObject = music
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    is ResultResource.Error -> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "error")
+                        }
+                    }
+                }
+            }
+
+    }
+}
+
---
+++ app/src/main/java/com/example/jetmusic/ViewModels/MainScreensViewModels/HomeViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.jetmusic.ViewModels.MainScreensViewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetmusic.DTOs.API.MusicDTOs.MusicResponse
+import com.example.jetmusic.Remote.Repositories.ApiRepostories.MusicRepository
+import com.example.jetmusic.Resources.ResultResource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val musicRepository: MusicRepository
+): ViewModel() {
+
+    private val _musicOfWeek: MutableStateFlow<ResultResource<MusicResponse>> =
+        MutableStateFlow(ResultResource.Loading())
+    val musicOfWeek: StateFlow<ResultResource<MusicResponse>> = _musicOfWeek.asStateFlow()
+
+   fun getMusicOfWeek(tags: String, offset: Int, ) = viewModelScope.launch {
+       musicRepository.getBestMusicOfWeek(tags, offset).collectLatest { result ->
+           _musicOfWeek.emit(result)
+       }
+   }
+
+    fun setMusicOfWeekLoading() = viewModelScope.launch {
+        _musicOfWeek.emit(ResultResource.Loading())
+    }
+}
