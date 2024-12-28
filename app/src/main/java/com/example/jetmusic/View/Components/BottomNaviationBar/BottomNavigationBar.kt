43612e91334d12cd9088bf6a92a17d8bfebfd4f3
ro-vezur <authenttesting@gmail.com>
2024-12-28 23:41:08 +0200

    feat: bottom navigation bar

--- app/src/main/java/com/example/jetmusic/View/Components/BottomNaviationBar/BottomNavigationBar.kt
+++ app/src/main/java/com/example/jetmusic/View/Components/BottomNaviationBar/BottomNavigationBar.kt
@@ -1,12 +1,13 @@
 package com.example.jetmusic.View.Components.BottomNaviationBar
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +17,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,10 +26,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.jetmusic.BOTTOM_NAVIGATION_BAR_HEIGHT
 import com.example.jetmusic.BOTTOM_NAVIGATION_BAR_ITEM_SIZE
+import com.example.jetmusic.View.Components.Icons.SelectedAnimatedIcon
 import com.example.jetmusic.View.ScreensRoutes
 import com.example.jetmusic.ui.theme.typography
 import ir.kaaveh.sdpcompose.sdp
@@ -36,12 +39,26 @@ import ir.kaaveh.sdpcompose.sdp
 fun BottomNavigationBar(
     navController: NavController
 ) {
-    var selected by remember { mutableStateOf(ScreensRoutes.HomeRoute) }
+    var selected by remember { mutableStateOf(BottomNavigationBarItems.HOME_SCREEN) }
+
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+
+    LaunchedEffect(currentBackStackEntry) {
+        val currentRoute = currentBackStackEntry?.destination?.route.toString()
+
+        BottomNavigationBarItems.entries.forEach { item ->
+            if(currentRoute.contains(item.title)) {
+                selected = item
+            }
+        }
+    }
 
     Box(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black.copy(0.75f)),
         contentAlignment = Alignment.Center
-    ){
+    ) {
         Row(
             horizontalArrangement = Arrangement.SpaceAround,
             verticalAlignment = Alignment.CenterVertically,
@@ -51,9 +68,16 @@ fun BottomNavigationBar(
                 .height(BOTTOM_NAVIGATION_BAR_HEIGHT.sdp)
         ) {
             BottomNavigationBarItems.entries.forEach { item ->
+                val isItemSelected = selected == item
+
                 BottomNavigationItem(
                     item = item,
-                    selected = selected == item.route
+                    selected = isItemSelected,
+                    select = {
+                        if(!isItemSelected) {
+                            navController.navigate(item.route)
+                        }
+                    }
                 )
             }
         }
@@ -64,27 +88,35 @@ fun BottomNavigationBar(
 private fun BottomNavigationItem(
     item: BottomNavigationBarItems,
     selected: Boolean,
+    select: () -> Unit,
 ) {
+    val color = if(selected) Color.White else Color.LightGray.copy(0.92f)
+
     Column(
         modifier = Modifier
             .clip(RoundedCornerShape(10.sdp))
             .width((BOTTOM_NAVIGATION_BAR_ITEM_SIZE + 16).sdp)
-            .fillMaxHeight(0.9f)
-            .clickable { },
+            .fillMaxHeight(1f)
+            .clickable { select() },
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
-        Icon(
-            imageVector = item.icon,
-            contentDescription = "bottom nav item",
+
+        SelectedAnimatedIcon(
             modifier = Modifier
-                .size(BOTTOM_NAVIGATION_BAR_ITEM_SIZE.sdp)
+                .size(BOTTOM_NAVIGATION_BAR_ITEM_SIZE.sdp),
+            isSelected = selected,
+            selectedIcon = item.selectedIcon,
+            unselectedIcon = item.unselectedIcon,
+            color = color
         )
 
         Text(
             modifier = Modifier
-                .padding(top = 1.sdp),
+                .padding(top = 2.sdp),
             text = item.title,
-            style = typography().bodySmall
+            style = typography().bodySmall,
+            color = animateColorAsState(targetValue = color).value,
         )
     }
 }
--- app/src/main/java/com/example/jetmusic/View/Components/BottomNaviationBar/BottomNavigationBarItems.kt
+++ app/src/main/java/com/example/jetmusic/View/Components/BottomNaviationBar/BottomNavigationBarItems.kt
@@ -2,14 +2,22 @@ package com.example.jetmusic.View.Components.BottomNaviationBar
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LibraryMusic
 import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.LibraryMusic
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.jetmusic.View.ScreensRoutes
 
-enum class BottomNavigationBarItems(val icon: ImageVector,val title: String,val route: Any) {
-    HOME_SCREEN(Icons.Filled.Home,"Home",ScreensRoutes.HomeRoute),
-    SEARCH_SCREEN(Icons.Filled.Search,"Search",ScreensRoutes.SearchRoute),
-    LIBRARY_SCREEN(Icons.Filled.List,"Library",ScreensRoutes.NowPlayingRoute),
+enum class BottomNavigationBarItems(
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val title: String,
+    val route: Any
+) {
+    HOME_SCREEN(Icons.Filled.Home,Icons.Outlined.Home,"Home",ScreensRoutes.HomeRoute),
+    SEARCH_SCREEN(Icons.Filled.Search,Icons.Outlined.Search,"Search",ScreensRoutes.SearchRoute),
+    LIBRARY_SCREEN(Icons.Filled.LibraryMusic,Icons.Outlined.LibraryMusic,"Library",ScreensRoutes.LibraryRoute),
 }
