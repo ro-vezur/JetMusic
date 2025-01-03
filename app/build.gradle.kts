cca5168c6c97fd113b981441da785b916652ef2a
ro-vezur <authenttesting@gmail.com>
2025-01-03 21:51:31 +0200

    feat: gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    id("kotlin-parcelize")
     id("com.google.gms.google-services")
 }
 
