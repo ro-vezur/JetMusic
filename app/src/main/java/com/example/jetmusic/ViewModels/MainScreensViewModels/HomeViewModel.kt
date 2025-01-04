da786d2214e39e8a874dd0f6f7082850f8b22fab
ro-vezur <authenttesting@gmail.com>
2025-01-04 21:12:59 +0200

    feat: home screen

--- app/src/main/java/com/example/jetmusic/ViewModels/MainScreensViewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/jetmusic/ViewModels/MainScreensViewModels/HomeViewModel.kt
@@ -3,19 +3,21 @@ package com.example.jetmusic.ViewModels.MainScreensViewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetmusic.data.DTOs.API.MusicDTOs.MusicResponse
-import com.example.jetmusic.data.Remote.Repositories.ApiRepostories.MusicRepository
 import com.example.jetmusic.Resources.ResultResource
+import com.example.jetmusic.domain.usecases.api.musicAPI.music.BestMusicOfWeekUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val musicRepository: MusicRepository,
+    private val getBestMusicOfWeekUseCase: BestMusicOfWeekUseCase,
 ): ViewModel() {
 
     private val _musicOfWeek: MutableStateFlow<ResultResource<MusicResponse>> =
@@ -23,7 +25,17 @@ class HomeViewModel @Inject constructor(
     val musicOfWeek: StateFlow<ResultResource<MusicResponse>> = _musicOfWeek.asStateFlow()
 
    fun getMusicOfWeek(tags: String, offset: Int, ) = viewModelScope.launch {
-       musicRepository.getBestMusicOfWeek(tags, offset).collectLatest { result ->
+       flow {
+           emit(ResultResource.Loading())
+           val response = getBestMusicOfWeekUseCase(tags,offset)
+           if(response.results.isNotEmpty()) {
+               emit(ResultResource.Success(data = response))
+           } else {
+               emit(ResultResource.Error(message = "empty result"))
+           }
+       }.catch { e ->
+           emit(ResultResource.Error(message = e.message.toString()))
+       }.collectLatest { result ->
            _musicOfWeek.emit(result)
        }
    }
