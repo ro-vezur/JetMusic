2d10e8a36a79128a4cce728ecab4a53137d2db64
ro-vezur <authenttesting@gmail.com>
2025-01-13 23:08:10 +0200

    feat: constants

--- app/src/main/java/com/example/jetmusic/Constants.kt
+++ app/src/main/java/com/example/jetmusic/Constants.kt
@@ -8,11 +8,17 @@ const val BOTTOM_MUSIC_PLAYER_HEIGHT = 75
 const val BASE_BUTTON_HEIGHT = 42
 const val BASE_BUTTON_WIDTH = 268
 
+const val REQUESTED_SEARCH_BAR_WIDTH = 200
+const val REQUESTED_SEARCH_BAR_ICON_WIDTH = 70
+
+const val NOT_REQUESTED_SEARCH_BAR_WIDTH = 235
+const val NOT_REQUESTED_SEARCH_BAR_ICON_WIDTH = 34
+
 const val USERS_COLLECTION = "users"
 
 const val API_KEY = BuildConfig.apiKey
 const val BASE_API_URL = "https://api.jamendo.com/v3.0/"
 const val RESPONSE_FORMAT = "json"
-const val OFFSET_PER_PAGE = 5
+const val OFFSET_PER_PAGE = 15
 
 const val PAGE_SIZE = 15
