35478717f7e3aaaa7a4d98bcb430c0fbe60caaf2
ro-vezur <authenttesting@gmail.com>
2025-01-13 23:09:36 +0200

    feat: api usecases

---
+++ app/src/main/java/com/example/jetmusic/domain/usecases/api/musicAPI/music/DiscoverSongsUseCase.kt
@@ -0,0 +1,19 @@
+package com.example.jetmusic.domain.usecases.api.musicAPI.music
+
+import com.example.jetmusic.data.Remote.API.ApiService
+import com.example.jetmusic.data.Remote.API.TOTAL_POPULARITY
+import javax.inject.Inject
+
+class DiscoverSongsUseCase @Inject constructor(
+    private val apiService: ApiService
+) {
+    suspend operator fun invoke(
+        tags: String,
+        offset: Int,
+        order: String = TOTAL_POPULARITY
+    ) = apiService.bestMusicsOfWeek(
+        tags = tags,
+        offset = offset,
+        order = order
+    )
+}
