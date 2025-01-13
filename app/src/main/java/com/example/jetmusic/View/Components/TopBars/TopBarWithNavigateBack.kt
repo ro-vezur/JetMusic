255734cd49567e4d1c00fb14ec280ad8c23b1cd3
ro-vezur <authenttesting@gmail.com>
2025-01-13 23:11:24 +0200

    feat: top bar component

---
+++ app/src/main/java/com/example/jetmusic/View/Components/TopBars/TopBarWithNavigateBack.kt
@@ -0,0 +1,53 @@
+package com.example.jetmusic.View.Components.TopBars
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import com.example.jetmusic.Extensions.NavigateExtensions.navigateBack
+import com.example.jetmusic.ui.theme.typography
+import ir.kaaveh.sdpcompose.sdp
+
+@Composable
+fun TopBarWithNavigateBack(
+    title: String,
+    turnBack: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .height(70.sdp)
+            .background(MaterialTheme.colorScheme.background),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = Icons.Filled.ArrowBackIosNew,
+            contentDescription = "back",
+            modifier = Modifier
+                .padding(start = 18.sdp)
+                .size(25.sdp)
+                .clip(RoundedCornerShape(6.sdp))
+                .clickable { turnBack() }
+        )
+
+        Text(
+            text = title,
+            style = typography().headlineSmall,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(start = 22.sdp)
+        )
+    }
+}
