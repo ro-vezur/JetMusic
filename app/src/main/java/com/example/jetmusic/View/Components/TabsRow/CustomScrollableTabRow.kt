dd820df107ac813cb2377cd2ebf48037b339ae62
ro-vezur <authenttesting@gmail.com>
2025-01-17 23:16:12 +0200

    performance fix: scrollable tab row

--- app/src/main/java/com/example/jetmusic/View/Components/TabsRow/CustomScrollableTabRow.kt
+++ app/src/main/java/com/example/jetmusic/View/Components/TabsRow/CustomScrollableTabRow.kt
@@ -3,12 +3,16 @@ package com.example.jetmusic.View.Components.TabsRow
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -16,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import com.example.jetmusic.ui.theme.tidalGradient
 import com.example.jetmusic.ui.theme.typography
 import ir.kaaveh.sdpcompose.sdp
 import kotlinx.coroutines.launch
@@ -23,10 +28,9 @@ import kotlinx.coroutines.launch
 @Composable
 fun CustomScrollableTabRow(
     modifier: Modifier = Modifier,
-    pagerState: PagerState,
+    selectedIndex: Int,
     items: List<String>,
     textColor: Color,
-    containerColor: Color,
     onClick: (index: Int) -> Unit,
 ) {
     val scope = rememberCoroutineScope()
@@ -34,40 +38,40 @@ fun CustomScrollableTabRow(
     ScrollableTabRow(
         modifier = modifier,
         edgePadding = 0.sdp,
-        selectedTabIndex = pagerState.currentPage, divider = {},
+        selectedTabIndex = selectedIndex,
+        divider = { },
         containerColor = Color.Transparent,
         indicator = { tabPositions ->
-            tabPositions.forEach {
-
-            }
+            Box(
+                modifier = Modifier
+                    .tabIndicatorOffset(tabPositions[selectedIndex])
+                    .height(2.sdp)
+                    .clip(RoundedCornerShape(16.sdp))
+                    .background(tidalGradient)
+            )
         }
     ) {
         items.forEachIndexed { index, item ->
-            val selected = pagerState.currentPage == index
+            val selected = selectedIndex == index
 
-            val color = if(selected) containerColor else Color.Transparent
-
-            Box(
+            Tab(
                 modifier = Modifier
-                    .padding(horizontal = 4.sdp)
-                    .width(70.sdp)
-                    .height(32.sdp)
-                    .clip(RoundedCornerShape(16.sdp))
-                    .background(color)
-                    .clickable {
-                        scope.launch {
-                            pagerState.animateScrollToPage(index)
-                            onClick(index)
-                        }
-                    },
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = item,
-                    style = typography().bodyMedium,
-                    color = textColor
-                )
-            }
+                    .width((45 + item.length * 6).sdp)
+                    .clip(RoundedCornerShape(16.sdp)),
+                selected = selected,
+                onClick = {
+                    scope.launch {
+                        onClick(index)
+                    }
+                },
+                text = {
+                    Text(
+                        text = item,
+                        style = typography().bodyMedium,
+                        color = textColor
+                    )
+                }
+            )
         }
     }
 }
