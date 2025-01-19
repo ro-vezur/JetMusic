27c86357f9e299eb316d34ab5edb671d4541127c
ro-vezur <authenttesting@gmail.com>
2025-01-19 22:48:54 +0200

    feat: artist card

--- app/src/main/java/com/example/jetmusic/View/Components/Cards/ArtistCards/ArtistCard.kt
+++ app/src/main/java/com/example/jetmusic/View/Components/Cards/ArtistCards/ArtistCard.kt
@@ -1,19 +1,26 @@
 package com.example.jetmusic.View.Components.Cards.ArtistCards
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import coil.compose.AsyncImage
+import com.example.jetmusic.R
 import com.example.jetmusic.ui.theme.typography
 import ir.kaaveh.sdpcompose.sdp
 
@@ -29,14 +36,33 @@ fun ArtistCard(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        AsyncImage(
-            model = artistImage,
-            contentDescription = "artist image",
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(CircleShape)
-        )
+        if(artistImage.isNotBlank()) {
+            AsyncImage(
+                model = artistImage,
+                contentDescription = "artist image",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(CircleShape)
+            )
+        } else {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(CircleShape)
+                    .background(Color.LightGray),
+                contentAlignment = Alignment.Center
+            ){
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(CircleShape),
+                    painter = painterResource(id = R.drawable.ic_empty_profile),
+                    contentDescription = "empty artist image",
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+        }
 
         Text(
             modifier = Modifier
